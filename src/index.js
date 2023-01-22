7bcf6ad108feaca6862fe52de955ae78e6805fb0
RileyWashington <110904984+RileyWashington@users.noreply.github.com>
2023-01-22 22:44:01 +0000

    Added unit conversion, wind speed, weather description, and weather icon

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 function showConditions(response) {
-    console.log(response.data);
-    document.querySelector("h1").innerHTML = response.data.name;
-    document.querySelector("#cityTemp").innerHTML = Math.round(
-      response.data.main.temp
-    );
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let iconElement = document.querySelector("#icon");
+
+    celsiusTemperature = response.data.main.temp;
+
+console.log(response.data);
+document.querySelector("h1").innerHTML = response.data.name;
+document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
   }
   
   function searchCity(event) {
@@ -13,9 +26,36 @@ function showConditions(response) {
     let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(`${apiURL}`).then(showConditions);
   }
+
+  function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+  
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+  }
+
+  function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  }
+  
+  let celsiusTemperature = null;
   
   let form = document.querySelector("form");
   form.addEventListener("submit", searchCity);
+
+  let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
   
   let currentDate = new Date();
   console.log(currentDate);
